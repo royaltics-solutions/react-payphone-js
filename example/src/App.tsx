e20ebcf1d311eb7c89fbf1079bdca8658b6ab663
Royaltics Djasc <dpto.comercial@royaltics.com>
2026-01-19 23:13:49 -0500

    Add keys

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -24,8 +24,8 @@ function App() {
 
         <div className="payment-section">
           <PayphoneButton
-            token="QsaUa3ldOsmW_vbPHLyRwS6s54Wg1NWNRYcK8TlxIytxdLRJjAD1WBDtRdRHcNBJRoNmAy7FBq9JtGxHq3wyBqz60KVLjH4VSHjSVnDowPWN4Xt1gCG8B6urdF6SaJxazVo0EGSeFa7FdXuuf9xOeXETXXc7XI8UCS2tgvX3Ln67dDDCej1K3qPimBrJjSvRt0RYK6s0GjMsiRdOOIR9GrFOasfkFIQjSszP8GVvsKalCL-AzKyHqu6WBQfdBgymDxSuysx7pP6FPryv0qLrCcmI9Rtgj3qXQNoS1BpttedlPZxkw8BBQCaI4n7WL4Dwk8suyw"
-            storeId="d69d3dc7-ff54-496f-b2f4-e24686c9093b"
+            token="XXXXXXXXXXXXXXXXXXXXXXXXX"
+            storeId="DDDDD-ff54-496f-b2f4-XXXXXX"
             //121651
             popup
             amount={11.20}
@@ -44,8 +44,8 @@ function App() {
 
          <div className="payment-section" style={{ marginTop: '20px' }}>
           <PayphoneButtonBox
-            token="QsaUa3ldOsmW_vbPHLyRwS6s54Wg1NWNRYcK8TlxIytxdLRJjAD1WBDtRdRHcNBJRoNmAy7FBq9JtGxHq3wyBqz60KVLjH4VSHjSVnDowPWN4Xt1gCG8B6urdF6SaJxazVo0EGSeFa7FdXuuf9xOeXETXXc7XI8UCS2tgvX3Ln67dDDCej1K3qPimBrJjSvRt0RYK6s0GjMsiRdOOIR9GrFOasfkFIQjSszP8GVvsKalCL-AzKyHqu6WBQfdBgymDxSuysx7pP6FPryv0qLrCcmI9Rtgj3qXQNoS1BpttedlPZxkw8BBQCaI4n7WL4Dwk8suyw"
-            storeId="d69d3dc7-ff54-496f-b2f4-e24686c9093b"
+            token="XXXXXXXXXXXXXXXXXXXXXXXXX"
+            storeId="DDDDD-ff54-496f-b2f4-XXXXXX"
             //121651
             popup
             amount={11.20}
